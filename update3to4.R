158d113b1c97cb00f36bbff592651e35efed1c9e
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2022-02-10 20:07:13 +0100

    update script

--- update3to4.R
+++ update3to4.R
@@ -5,10 +5,14 @@ library(ncdf4)
 library(nctools)
 library(kali)
 
-base = "osmose-ben-v4-devel"
-cfg = "osmose-ben-v3.2-Florance/BEN_all-parameters-v3.2.csv"
+# manual inputs
+base    = "osmose-ben-v4-devel"
+cfg     = "osmose-ben-v3.2-Florance/BEN_all-parameters-v3.2.csv"
+ltl_new = "roms_climatological-%s_benguela_15days_2000_2009.nc" # regex
 
+plk_rename = c(Dinoflagellates="sphy", Diatoms="lphy", Ciliates="szoo", Copepods="lzoo")
 
+# start
 output = file.path(base, "osmose-ben.R")
 
 .readConfiguration = osmose.extras:::.readConfiguration
@@ -28,6 +32,10 @@ cat("# Setting the model -------------------------------------------------------
 plk = .getPar(ben, "plankton.name")
 spp = .getPar(ben, "species.name")
 fsh =  .getPar(ben, "mortality.fishing.rate.sp")
+
+if(exists("plk_rename")) {
+  plk = lapply(plk, function(x) setNames(plk_rename[x], NULL))
+}
 
 nspp = .getPar(ben, "nspecies")
 nplk = length(plk)
@@ -96,8 +104,185 @@ out1[["grid.var.mask"]]       = "mask"
 write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
 
 
+cat("\n# Resource configuration --------------------------------------------------\n", file=output, append = TRUE)
 
+ltl_file = .getPar(ben, "ltl.netcdf.file")
+ltl_file = file.path(attr(ltl_file, "path"), ltl_file)
 
+plknames = unlist(all_sp)[unlist(all_type)=="resource"]
 
+for(i in seq_len(nplk)) {
+  iplk = plknames[i]
+  nc_subset(ltl_file, varid="ltl_biomass", newvarid = iplk, drop=TRUE,
+            output=file.path(base, "input", sprintf(ltl_new, iplk)), ltl=i)
+}
 
+plkfiles = file.path("input", sprintf(ltl_new, plknames))
+names(plkfiles) = gsub(names(plknames), pattern="name", replacement="file")
+plkfiles = as.list(plkfiles)
 
+write_osmose(as.matrix(plkfiles), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+out1 = list(species.biomass.nsteps.year=.getPar(ben, "ltl.nstep"))
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+out1 = .getPar(ben, "plankton.accessibility2fish")
+names(out1) = gsub(names(plknames), pattern="name", replacement="accessibility2fish")
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+cat("\n# Resource sizes", file=output, append = TRUE)
+
+out1 = .getPar(ben, "plankton.size.min")
+names(out1) = gsub(names(plknames), pattern="name", replacement="size.min")
+out2 = .getPar(ben, "plankton.size.max")
+names(out2) = gsub(names(plknames), pattern="name", replacement="size.max")
+out1 = c(out1, out2)
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+cat("\n# Resource Trophic Level", file=output, append = TRUE)
+
+out1 = .getPar(ben, "plankton.tl")
+names(out1) = gsub(names(plknames), pattern="name", replacement="TL")
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+cat("\n# Resource Multiplier", file=output, append = TRUE)
+
+out1 = rep(1, nplk)
+names(out1) = gsub(names(plknames), pattern="name", replacement="multiplier")
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+cat("\n# Species configuration ---------------------------------------------------\n", file=output, append = TRUE)
+
+cat("\n# Growth: von Bertalanffy growth model", file=output, append = TRUE)
+
+pars = c("species.lInf", "species.K", "species.t0", "species.vonbertalanffy.threshold.age",
+         "species.length2weight.condition.factor", "species.length2weight.allometric.power",
+         "species.lifespan")
+
+for(ipar in pars) {
+  write_osmose(as.matrix(.getPar(ben, ipar)), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+}
+
+cat("\n# Reproduction", file=output, append = TRUE)
+
+write_osmose(as.matrix(.getPar(ben, "species.relativefecundity")), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+rep_files = .getPar(ben, "reproduction.season.file")
+rep_files = sapply(rep_files, function(x) file.path(attr(x, "path"), x))
+
+dir.create(file.path(base, "input", "reproduction"), recursive = TRUE)
+file.copy(rep_files, to=file.path(base, "input", "reproduction"))
+
+rep_files = .getPar(ben, "reproduction.season.file")
+rep_files = lapply(rep_files, function(x) file.path("input/reproduction", x))
+
+write_osmose(as.matrix(rep_files), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+# update maturity length
+
+mat_age = .getPar(ben, "species.maturity.age")
+if(!is.null(mat_age)) {
+
+  mat = spp
+  names(mat) = gsub(names(mat), pattern="name", replacement = "maturity.size")
+
+  for(i in seq_len(nspp) - 1) {
+    this = .getPar(ben, sp=i)
+    len = .getPar(this, "species.maturity.size")
+    if(!is.null(len)) {
+      mat[[i+1]] = len
+    } else {
+      age = .getPar(this, "species.maturity.age")
+      mat[[i+1]] = round(osmose.extras:::VB(age, this), 1)
+    }
+  }
+}
+
+pars = c("species.egg.size", "species.egg.weight", "species.sexratio")
+
+for(ipar in pars) {
+  write_osmose(as.matrix(.getPar(ben, ipar)), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+}
+
+write_osmose(as.matrix(mat), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+cat("\n# Survival", file=output, append = TRUE)
+
+out1 = list(mortality.subdt = .getPar(ben, "mortality.subdt"))
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+out1 = .getPar(ben, "mortality.natural.rate")
+names(out1) = gsub(names(out1), pattern="natural", replacement = "additional")
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+out1 = .getPar(ben, "mortality.natural.larva.rate")
+names(out1) = gsub(names(out1), pattern="natural", replacement = "additional")
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+out1 = .getPar(ben, "mortality.starvation.rate")
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+out1 = .getPar(ben, "population.seeding.biomass")
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+out1 = list(population.seeding.year=.getPar(ben, "population.seeding.year"))
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+cat("\n# Predation configuration -------------------------------------------------\n", file=output, append = TRUE)
+
+acc_file = .getPar(ben, "predation.accessibility.file")
+acc_file = file.path(attr(acc_file, "path"), acc_file)
+
+acc = read.csv2(acc_file, check.names = FALSE)
+
+if(exists("plk_rename")) {
+  new = plk_rename[acc[,1]]
+  acc[!is.na(new), 1] = new[!is.na(new)]
+}
+
+write.csv(acc, file=file.path(base, "input", basename(acc_file)), quote = FALSE, row.names = FALSE)
+
+out1 = .getPar(ben, "predation.accessibility.stage")
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+pars = c("predation.efficiency.critical", "predation.ingestion.rate.max")
+
+for(ipar in pars) {
+  write_osmose(as.matrix(.getPar(ben, ipar)), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+}
+
+pp = "predation.predPrey.sizeRatio"
+for(i in seq_len(nspp)-1) {
+  ipar = .getPar(ben, sp=i)
+  xx = .getPar(ipar, pp)
+  xx = lapply(xx, FUN=paste, collapse=", ")
+  write_osmose(as.matrix(xx), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+}
+
+out1 = .getPar(ben, "predation.predPrey.stage")
+write_osmose(as.matrix(out1), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+
+cat("\n# Movement configuration --------------------------------------------------\n", file=output, append = TRUE)
+
+pars = c("movement.distribution.method", "movement.randomwalk.range")
+
+for(ipar in pars) {
+  write_osmose(as.matrix(.getPar(ben, ipar)), file=output, append=TRUE, col.names = FALSE, sep=" = ")
+}
+
+### ADD MAPS CONFIGURATION
+
+#####
+
+cat("\n# Fisheries configuration -------------------------------------------------\n", file=output, append = TRUE)
+
+# copy grid as fishing area (check is one map per fishery, run humboldt)
+# create catchabiilty and discards file
+# write each fishery (knife-edge)
+
+cat("\n# Output configuration ----------------------------------------------------\n", file=output, append = TRUE)
+
+# keep main ones
+# copy my own (create list, all FALSE)
+
+cat("\n# Advanced parameters -----------------------------------------------------\n", file=output, append = TRUE)
