0968c249239007330eec920312bca8455eb529f9
Ricardo Oliveros-Ramos <ricardo.oliveros@gmail.com>
2022-02-16 02:06:14 +0100

    updated launcher.

--- launcher.R
+++ launcher.R
@@ -16,6 +16,7 @@ outputDir4  = file.path(configDir4, "output")
 run_osmose(input = configFile4, output = outputDir4, osmose = jarFile, version = "4.3.3")
 
 ben = read_osmose(path = outputDir4, version = "4.3.3")
-plot(ben, initialYear=1992)
+plot(ben, initialYear=2000)
+plot(ben, initialYear=2000, xlim=c(2080, 2100))
 plot(ben, what = "yield", initialYear=1992)
 
